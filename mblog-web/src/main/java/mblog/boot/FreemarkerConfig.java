278ab34308dd82692c6ca82d8e7187106dcfa7b0
langhsu <langhsu@163.com>
2017-11-24 11:15:10 +0800

    替换后台模板

--- mblog-web/src/main/java/mblog/boot/FreemarkerConfig.java
+++ mblog-web/src/main/java/mblog/boot/FreemarkerConfig.java
@@ -3,10 +3,7 @@ package mblog.boot;
 import freemarker.template.Configuration;
 import freemarker.template.TemplateModelException;
 import mblog.shiro.tags.ShiroTags;
-import mblog.template.directive.AuthorContentsDirective;
-import mblog.template.directive.ContentsDirective;
-import mblog.template.directive.NumberDirective;
-import mblog.template.directive.ResourceDirective;
+import mblog.template.directive.*;
 import mblog.template.method.TimeAgoMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -30,6 +27,7 @@ public class FreemarkerConfig {
         configuration.setSharedVariable("contents", applicationContext.getBean(ContentsDirective.class));
         configuration.setSharedVariable("num", applicationContext.getBean(NumberDirective.class));
         configuration.setSharedVariable("resource", applicationContext.getBean(ResourceDirective.class));
+        configuration.setSharedVariable("authc", applicationContext.getBean(AuthcDirective.class));
 
         configuration.setSharedVariable("timeAgo", new TimeAgoMethod());
         configuration.setSharedVariable("shiro", new ShiroTags());
