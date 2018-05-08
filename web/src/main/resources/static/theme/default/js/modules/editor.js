abc528d987e8424da319a585301b353e7f7a41cb
langhsu <langhsu@163.com>
2018-05-08 15:29:49 +0800

    editor 支持图片黏贴上传 @杭州-峰

--- web/src/main/resources/static/theme/default/js/modules/editor.js
+++ web/src/main/resources/static/theme/default/js/modules/editor.js
@@ -35,7 +35,7 @@ define(function(require, exports, module) {
                 codesample_dialog_width: '600',
                 codesample_dialog_height: '400',
                 block_formats: 'Paragraph=p;标题1=h4;标题2=h5;标题3=h6;Preformatted=pre',
-
+                paste_data_images: true,
                 content_css: [
                     window.app.base + '/dist/vendors/bootstrap/css/bootstrap.min.css',
                     window.app.base + '/theme/default/css/editor.css',
