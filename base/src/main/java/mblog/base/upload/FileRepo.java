acb77673c00ac263e5dc75335d9c6eae822a848e
langhsu <langhsu@163.com>
2018-05-31 14:48:14 +0800

    优化图片上传及存储逻辑

--- base/src/main/java/mblog/base/upload/FileRepo.java
+++ base/src/main/java/mblog/base/upload/FileRepo.java
@@ -11,7 +11,6 @@ package mblog.base.upload;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -20,26 +19,6 @@ import java.io.IOException;
  */
 public interface FileRepo {
 	/**
-	 * 保存临时图片
-	 * - 临时图片不会生成复杂的多级目录
-	 * @param file
-	 * @param basePath
-	 * @return
-	 * @throws IOException
-	 */
-	String temp(MultipartFile file, String basePath) throws IOException;
-	
-	/**
-	 * 保存压缩后的临时图片
-	 * - 临时图片不会生成复杂的多级目录
-	 * @param file
-	 * @param basePath
-	 * @return
-	 * @throws IOException
-	 */
-	String tempScale(MultipartFile file, String basePath, int maxWidth) throws Exception;
-	
-	/**
 	 * 存储图片
 	 * @param file
 	 * @param basePath
@@ -47,15 +26,6 @@ public interface FileRepo {
 	 * @throws IOException
 	 */
 	String store(MultipartFile file, String basePath) throws IOException;
-	
-	/**
-	 * 存储图片
-	 * @param file
-	 * @param basePath
-	 * @return
-	 * @throws IOException
-	 */
-	String store(File file, String basePath) throws IOException;
 	
 	/**
 	 * 存储压缩图片
@@ -74,24 +44,6 @@ public interface FileRepo {
 	 * @throws IOException
 	 */
 	String storeScale(MultipartFile file, String basePath, int width, int height) throws Exception;
-	
-	/**
-	 * 存储压缩图片
-	 * @param file
-	 * @param basePath
-	 * @return
-	 * @throws IOException
-	 */
-	String storeScale(File file, String basePath, int maxWidth) throws Exception;
-
-	/**
-	 * 存储压缩图片
-	 * @param file
-	 * @param basePath
-	 * @return
-	 * @throws IOException
-	 */
-	String storeScale(File file, String basePath, int width, int height) throws Exception;
 	
 	/**
 	 * 获取图片大小
